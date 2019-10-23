fa728e3cb5c11ceb4ef2cd9fdc37bfe103205179
Yuliya Maximchikova <m.yuliyas.00@gmail.com>
2019-10-23 08:34:50 +0300

    1-8

---
+++ 09-08.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const fs = require('fs');
+
+const file = fs.createWriteStream('./files/MyFile.txt');
+
+let options = {
+    host: 'localhost',
+    path: '/files/MyFile.txt',
+    port: 3000,
+    method: 'GET',
+};
+
+const req = http.request(options, (res) => {
+    res.pipe(file);
+   });
+
+req.on('error', (err) => {
+    console.log('http.request: error: ', err.message);
+});
+
+req.end();
